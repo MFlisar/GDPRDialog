fb987c398fbbbe8724dbedc1bf03613d0f5fff04
MFlisar <mflisar.development@gmail.com>
2018-05-25 20:00:30 +0200

    java docs for consent enums added

--- library/src/main/java/com/michaelflisar/gdprdialog/GDPRConsent.java
+++ library/src/main/java/com/michaelflisar/gdprdialog/GDPRConsent.java
@@ -1,10 +1,25 @@
 package com.michaelflisar.gdprdialog;
 
 public enum GDPRConsent {
+    /**
+     * users consent is unknown, it needs to be requests
+     */
     UNKNOWN,
+    /**
+     * user consent given: he does not accept any usage of personal data
+     */
     NO_CONSENT,
+    /**
+     * user consent given: he accept non personal data only
+     */
     NON_PERSONAL_CONSENT_ONLY,
+    /**
+     * user consent given: he accepts personal data usage
+     */
     PERSONAL_CONSENT,
+    /**
+     * user consent automatically set because of request location: personal data usage is possible
+     */
     AUTOMATIC_PERSONAL_CONSENT;
 
     public boolean isPersonalConsent() {
